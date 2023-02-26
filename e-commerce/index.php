48bbd675f3c2aed5a4e41eca99253f9d767e407e
Tales Oliver <talis.oliveira@hotmail.com.br>
2023-02-26 19:35:40 -0300

    criando a classe page

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -1,21 +1,18 @@
 <?php
 
-use DB\Sql;
-
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use Hcode\Page;
+use Slim\Slim;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
 $app->get('/', function() {
     
-	echo "OK, viu";
-	$sql = new Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
+	$page = new Page();
+	$page->setTpl("index");
 
 });
 
