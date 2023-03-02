7ed28d75596a3295ce40b996c62df9181ba7b0fc
Tales Oliver <talis.oliveira@hotmail.com.br>
2023-03-01 21:03:49 -0300

    autenticao de login com falhas

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -4,6 +4,7 @@ require_once("vendor/autoload.php");
 
 use Hcode\Page;
 use Hcode\PageAdmin;
+use Hcode\Model\User;
 use Slim\Slim;
 
 $app = new Slim();
@@ -22,6 +23,23 @@ $app->get('/admin', function() {
 	$page = new PageAdmin();
 	$page->setTpl("index");
 
+});
+
+$app->get('/admin/login', function(){
+	
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$page->setTpl("login");
+
+});
+
+$app->post('/admin/login', function(){
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
 });
 
 $app->run();
