643838cf5390963f6604000339e66c8cce8a0866
Tales Oliver <talis.oliveira@hotmail.com.br>
2023-03-06 20:02:15 -0300

    crud dos usuarios

--- e-commerce/index.php
+++ e-commerce/index.php
@@ -70,25 +70,63 @@ $app->get("/admin/users/create", function(){
 	$page->setTpl("users-create");
 });
 
-$app->get("/admin/user/:iduser/delete", function($iduser){
+$app->get("/admin/users/:iduser/delete", function($iduser){
 	User::verifyLogin();
+
+	$user = new User();
+
+	$user->get((int)$iduser);
+
+	$user->delete();
+
+	header("Location: /admin/users");
+	exit;
 
 });
 
 $app->get("/admin/users/:iduser", function($iduser){
 	User::verifyLogin();
 
+	$user = new User();
+
+	$user->get((int)$iduser);
+
 	$page = new PageAdmin();
-	$page->setTpl("users-update");
+	$page->setTpl("users-update", array(
+		"user"	=>	$user->getValues()
+	));
 });
 
-$app->post("/admin/user/create", function(){
+$app->post("/admin/users/create", function(){
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST)) ? 1 : 0;
+
+	$user->setData($_POST);
+
+	$user->save();
+
+	header("Location: /admin/users");
 
 });
 
-$app->post("/admin/user/:iduser", function($iduser){
+$app->post("/admin/users/:iduser", function($iduser){
 	User::verifyLogin();
+
+	$user = new User();
+
+	$_POST["inadmin"] = (isset($_POST)) ? 1 : 0;
+
+	$user->get((int)$iduser);
+
+	$user->setData($_POST);
+
+	$user->update();
+
+	header("Location: /admin/users");
+	exit;
 
 });
 
